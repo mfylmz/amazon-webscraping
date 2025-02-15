1cb070c66f9d72f1a8524cfafccd094b78b3f5ce
mfylmz <m.furkanyilmaz21@gmail.com>
2025-02-15 07:13:46 -0600

    Updated version

--- webscraper.py
+++ webscraper.py
@@ -1,33 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+url = "https://www.imdb.com/chart/top/"
 
-url = "https://www.amazon.ca/s?k=video+game+collectibles&crid=2T2LIDY1GU6K8&sprefix=video+game%2Caps%2C113&ref=nb_sb_ss_mvt-t11-ranker_7_10"
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-    "Accept-Language": "en-US,en;q=0.9",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
 }
 
-response = requests.get(url, headers=headers)
 
-if response.status_code == 200:
-    soup = BeautifulSoup(response.text, "html.parser")
+request = requests.get(url, headers=headers)
+soup = BeautifulSoup(request.text, "html.parser")
+
+movie_list=[]
+
+movies = soup.find_all('li', class_="ipc-metadata-list-summary-item")
+
+for movie in movies:
+    title_tag=movie.find('h3', class_='ipc-title__text')
+    title = title_tag.text.strip()
+    metadata = movie.find_all('span', class_="cli-title-metadata-item")
+    year = metadata[0].text.strip()
+    duration = metadata[1].text.strip()
     
-    title = soup.find("span", {"id": "productTitle"})
-    title = title.get_text(strip=True) if title else "Bilinmeyen Ürün"
+    rating_tag = movie.find('span', class_="ipc-rating-star--imdb")
+    rating = rating_tag.text.strip()
+    
+    movie_list.append([title, year, duration, rating])
 
-    # Fiyat (bazı ürünlerde farklı HTML yapı olabilir, bu yüzden birkaç seçenek deneyelim)
-    price = soup.find("span", {"class": "a-price-whole"})
-    price = price.get_text(strip=True) if price else "Fiyat Bulunamadı"
 
-    # Veriyi DataFrame'e ekle
-    df = pd.DataFrame([[title, price]], columns=["Ürün Adı", "Fiyat"])
 
-    # Excel'e kaydet
-    df.to_excel("amazon_urunleri.xlsx", index=False)
+    
+df= pd.DataFrame(movie_list, columns=["Movie Name", "Release Date", "Duration", "Rating"])
+df.to_excel("imdb_ratings.xlsx", index=False)
 
-    print(f"Ürün: {title}, Fiyat: {price}")
-    print("Excel dosyası kaydedildi: amazon_urunleri.xlsx")
-else:
-    print("Veri çekme başarısız! Amazon bot koruması olabilir.")
+print("Imdb List Created")
